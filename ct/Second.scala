d916c589afbf6f1acfd6404bb487e9806282c292
Gleb Lobov <lobzison@gmail.com>
2019-09-29 23:32:32 +0300

    big oof

---
+++ ct/Second.scala
@@ -0,0 +1,12 @@
+// 1 O(b)
+// 2 O(b*lga^2) if multiplication is O(1) - b
+// 3 if division is O(1) - O(1)
+// 4 O(a/b)
+// 5 O(logn)
+// 6 O(sqrt(n))
+// 7 O(h) where h is the height of the tree on average, O(n) the worst
+// 8 O(n) where n is number of nodes
+// 9 O(n^2)
+// O(lgn)
+// O(kc^n)
+// O((a + b)logb)
